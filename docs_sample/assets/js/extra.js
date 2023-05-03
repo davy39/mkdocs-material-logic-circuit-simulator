c2abe2a95868559f164e7bc6d6f1d15ae253a427
Davy <davy@ik.me>
2023-05-03 21:10:57 +0200

    Update extra.js

--- docs_sample/assets/js/extra.js
+++ docs_sample/assets/js/extra.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-    // Check out what theme is set when loading the page
-    if ($('body').attr('data-md-color-scheme') === 'slate') {
-        Logic.setDarkMode(true);
-    };
-    if ($('body').attr('data-md-color-scheme') === 'default') {
-        Logic.setDarkMode(false);
-      };
-    // Toggle if theme changed
-    $('input[data-md-color-media="(prefers-color-scheme: dark)"]').change(function() {
-        Logic.setDarkMode(true);
-    });
-    $('input[data-md-color-media="(prefers-color-scheme: light)"]').change(function() {
-        Logic.setDarkMode(false);
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  var body = document.body;
+  if (body.getAttribute('data-md-color-scheme') === 'slate') {
+    Logic.setDarkMode(true);
+  } else if (body.getAttribute('data-md-color-scheme') === 'default') {
+    Logic.setDarkMode(false);
+  }
+  var darkInput = document.querySelector('input[data-md-color-media="(prefers-color-scheme: dark)"]');
+  var lightInput = document.querySelector('input[data-md-color-media="(prefers-color-scheme: light)"]');
+  darkInput.addEventListener('change', function() {
+    Logic.setDarkMode(true);
+  });
+  lightInput.addEventListener('change', function() {
+    Logic.setDarkMode(false);
+  });
 });
